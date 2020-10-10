77a7ecec29b5683030591d9e87084f631e77ea1c
MusicalMonarch <alyssalahoy@gmail.com>
2020-10-10 13:10:30 -0700

    fixing some css to make more responsive

--- lesson4/js/script.js
+++ lesson4/js/script.js
@@ -3,3 +3,9 @@ function toggleMenu() {
     console.log(document.getElementById("primaryNav").classList);
     document.getElementById("primaryNav").classList.toggle("hide");
 }
+
+let date = new Date();
+let modified = document.lastModified;
+
+document.getElementById("copyright-date").innerHTML = date.getFullYear();
+document.getElementById("update-date").innerHTML = modified;
