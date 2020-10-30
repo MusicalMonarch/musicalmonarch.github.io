bc9571cea0362be71142d31a9cb6b4d3e82de2bb
MusicalMonarch <alyssalahoy@gmail.com>
2020-10-30 16:16:28 -0700

    adds pictures and grid layout for gallery

--- lesson7/js/script.js
+++ lesson7/js/script.js
@@ -9,7 +9,3 @@ let modified = document.lastModified;
 
 document.getElementById("copyright-date").innerHTML = date.getFullYear();
 document.getElementById("update-date").innerHTML = modified;
-
-if (dayOfWeek == 5) {
-    document.getElementById("announcement-alert").style.display = "block";
-}
