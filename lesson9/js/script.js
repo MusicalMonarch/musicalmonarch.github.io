2400f041083cf1d3e642153647111baa6c8264e4
MusicalMonarch <alyssalahoy@gmail.com>
2020-11-19 14:07:44 -0800

    commits small changes to storm center page

--- lesson9/js/script.js
+++ lesson9/js/script.js
@@ -3,6 +3,10 @@ function toggleMenu() {
     document.getElementById("primaryNav").classList.toggle("hide");
 }
 
+function updateTextInput(val) {
+    document.getElementById('severityVal').innerHTML = "<b>" + val + "</b>"; 
+}
+
 let date = new Date();
 let dayOfWeek = date.getDay();
 let modified = document.lastModified;
