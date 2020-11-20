7d86ce2d2d41d1c838e8cf72b4c7d6b0f3c3d1cd
MusicalMonarch <alyssalahoy@gmail.com>
2020-11-19 16:15:35 -0800

    fixes image address

--- lesson10/js/weatherapi.js
+++ lesson10/js/weatherapi.js
@@ -8,7 +8,7 @@ fetch(apiURL)
         //changes the "current-temp" span text to the value of the object from the api, the object containing a "main" which contains a "temp"
         document.getElementById('current-temp').textContent = jsObject.main.temp;
         //creates a variable that stores the icon and concatenates with the url to make the image address
-        const imageSRC = 'https://openweathermap.org/img/w' + jsObject.weather[0].icon + '.png';
+        const imageSRC = 'https://openweathermap.org/img/w/' + jsObject.weather[0].icon + '.png';
         //store the weather description
         const desc = jsObject.weather[0].description;
         //use the const variables to put the values in the html
