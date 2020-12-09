493ca4c9e8613a8d9aa48b72cd3f9a230bad04c5
MusicalMonarch <alyssalahoy@gmail.com>
2020-12-08 19:31:09 -0800

    adds more content to home page and sets up json file

--- scoots/scripts/basics.js
+++ scoots/scripts/basics.js
@@ -1,3 +1,10 @@
+
+//hides nav button
+function toggleMenu() {
+    document.getElementById('primaryNav').classList.toggle('hide');
+}
+
+//gets the date and shows when website was last updated
 let date = new Date();
 let dayOfWeek = date.getDay();
 let modified = document.lastModified;
