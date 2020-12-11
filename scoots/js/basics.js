a5f7169885768086d3e1a53e8f9611845d7afe74
MusicalMonarch <alyssalahoy@gmail.com>
2020-12-10 21:19:15 -0800

    add more css to home page and adds rental images

--- scoots/js/basics.js
+++ scoots/js/basics.js
@@ -1,7 +1,17 @@
 
-//hides nav button
+//function to toggle nav when in small view
 function toggleMenu() {
     document.getElementById('primaryNav').classList.toggle('hide');
+    let navBtn = document.getElementById('navBtn');
+
+    //toggle the nav button appearance
+    if(navBtn.classList.contains('fa-angle-down')) {
+        navBtn.classList.remove('fa-angle-down');
+        navBtn.classList.add('fa-angle-up');
+    } else {
+        navBtn.classList.remove('fa-angle-up');
+        navBtn.classList.add('fa-angle-down');
+    }
 }
 
 //gets the date and shows when website was last updated
--- scoots/js/rentalex.js
+++ scoots/js/rentalex.js
@@ -9,4 +9,20 @@ fetch(requestURL).then(function (response) {
     //create a variable for the jsonObject
     const rentals = jsonObject["rentalTypes"];
     console.log(rentals);
+    //for loop that goes on for three products just for the home page
+    for(let i = 0; i < 3; i++) {
+        //create HTML elements that are going to be used in this section
+        let card = document.createElement('div');
+        let rentalName = document.createElement('h3');
+        let rentalImg = document.createElement('img');
+        //place json data into text and attributes with the elements
+        rentalName.textContent = rentals[i].name;
+        rentalImg.setAttribute('src', rentals[i].imageurl);
+        rentalImg.setAttribute('alt', rentals[i].name);
+        //append elements to card
+        card.appendChild(rentalImg);
+        card.appendChild(rentalName);
+        //append card to appropriate section
+        document.getElementById('rentals-ex').appendChild(card);
+    }
 })
