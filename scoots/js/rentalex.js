4c3bfdd9e69a2a5c8664e73df0ae1c7a8283e0c4
MusicalMonarch <alyssalahoy@gmail.com>
2020-12-10 19:11:22 -0800

    adds more css to home page and finishes json file content

---
+++ scoots/js/rentalex.js
@@ -0,0 +1,15 @@
+
+/*function readJSON(path) {
+    var getJson = new XMLHttpRequest();
+    getJson.open('GET', path, true);
+    getJson.responseType = 'blob';
+    getJson.onload = function(e) {
+        if(this.status == 200) {
+            var file = new File([this.response], 'temp');
+            var fileReader = new FileReader();
+            fileReader.addEventListener('load', function(){
+
+            })
+        }
+    }
+}*/
