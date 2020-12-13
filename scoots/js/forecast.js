b4f672ed55dea1195b87aced2c738ee2361fe34b
MusicalMonarch <alyssalahoy@gmail.com>
2020-12-13 10:32:27 -0800

    add content and css to home page

--- scoots/js/forecast.js
+++ scoots/js/forecast.js
@@ -6,7 +6,9 @@ fetch(apiURL)
     .then((jsObject) => {
         console.log(jsObject);
         //set spans to actual data that updates
-        document.getElementById('weather-desc').textContent = jsObject.current.weather[0].description;
+        let description = jsObject.current.weather[0].description;
+        let weatherDesc = description.charAt(0).toUpperCase() + description.slice(1);
+        document.getElementById('weather-desc').textContent = weatherDesc;
         document.getElementById('current-temp').textContent = Math.round(jsObject.current.temp);
         document.getElementById('current-humid').textContent = jsObject.current.humidity;
         
@@ -14,10 +16,29 @@ fetch(apiURL)
         //create an array to store weekdays
         const weekdays = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
         //for loop to create content for forecast
-        for(let i = 0; i < 3; i++) {
+        for(let i = 1; i < 4; i++) {
             //create variables to store data and create elements
-            const foreDate = new Date(jsObject.daily.dt * 1000);
-            let weekday = foreDate.getDay();
-            console.log(weekday);
+            const foreDate = new Date(jsObject.daily[i].dt * 1000);
+            let iconSrc = 'https://openweathermap.org/img/w/' + jsObject.daily[i].weather[0].icon + '.png';
+            let desc = jsObject.daily[i].weather[0].description;
+            let forecastCard = document.createElement('li');
+            let weekday = document.createElement('h3');
+            let forecastIcon = document.createElement('img');
+            let forecastTemp = document.createElement('p');
+
+            //store content in elements
+            weekday.textContent = weekdays[foreDate.getDay()];
+            forecastIcon.setAttribute('src', iconSrc);
+            forecastIcon.setAttribute('alt', desc);
+            forecastIcon.style.width = '4.4em';
+            forecastTemp.innerHTML = Math.round(jsObject.daily[i].temp.day) + '&#176;F';
+
+            //append elements to li
+            forecastCard.appendChild(weekday);
+            forecastCard.appendChild(forecastIcon);
+            forecastCard.appendChild(forecastTemp);
+            forecastCard.classList.add('cards');
+
+            document.getElementById('threeForecast').appendChild(forecastCard);
         }
     })
